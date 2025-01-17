6dcac7f4494743c4be40d0d6bb5308c4acba11b8
Aniruddha Ghosh <ghoshaniruddha2003@gmail.com>
2025-01-17 09:14:07 +0530

    more routes added

--- Routes/demandRoutes.py
+++ Routes/demandRoutes.py
@@ -9,9 +9,46 @@ demandApi = Blueprint('demand', __name__)
 db_config = {
     'user': 'admin',
     'password': 'Babai123',
-    'host': 'guvnl.cv4e0cyc8rtk.ap-south-1.rds.amazonaws.com',
+    'host': 'guvnl-dev.cv4e0cyc8rtk.ap-south-1.rds.amazonaws.com',
     'database': 'guvnl_dev'
 }
+
+
+@demandApi.route('/dashboard', methods=['GET'])
+def get_dashboard_data():
+    try:
+        # Establish a connection to the database
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor(dictionary=True)
+
+        # SQL query to count rows in the `plant_details` table
+        cursor.execute("SELECT COUNT(TYPE) AS count FROM `plant_details`")
+        plant_count = cursor.fetchone()
+
+        cursor.execute(
+            '''SELECT SUM(`Demand(Actual)`) AS total_demand_actual, SUM(`Demand(Pred)`) AS total_demand_predicted FROM `demand_data`;''')
+        demand_data = cursor.fetchone()
+
+        cursor.execute('''SELECT AVG(`Cost_Per_Block`) AS average_pred_price FROM `demand_output`;''')
+        avg_price = cursor.fetchone()
+
+        # Close cursor and connection
+        cursor.close()
+        conn.close()
+
+        # Ensure the result is accessed correctly and returned
+        if plant_count:
+            return jsonify({"plant_count": plant_count["count"],
+                            "demand_actual": round(float(demand_data['total_demand_actual']), 3),
+                            "demand_predicted": round(float(demand_data['total_demand_predicted']), 3),
+                            "avg_price": round(float(avg_price['average_pred_price']), 2)},
+                           ), 200
+        else:
+            return jsonify({"error": "No data found"}), 404
+
+    except mysql.connector.Error as err:
+        # Handle MySQL connection errors
+        return jsonify({"error": str(err)}), 500
 
 
 @demandApi.route('/all', methods=['GET'])
--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from Routes.demandRoutes import demandApi
-from Routes.plantRoutes import plantAPI
-from Routes.procurementRoutes import procurementAPI
+from Routes.procurementRoutes import plantAPI
+from Routes.plantRoutes import procurementAPI
+from Routes.consumerRoutes import consumerAPI
 import requests
 
 app = Flask(__name__)
@@ -12,6 +13,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})  # Enable CORS for all routes and
 app.register_blueprint(procurementAPI, url_prefix='/procurement')
 app.register_blueprint(plantAPI, url_prefix='/plant')
 app.register_blueprint(demandApi, url_prefix='/demand')
+app.register_blueprint(consumerAPI, url_prefix='/consumer')
 
 
 @app.route('/')
@@ -20,4 +22,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4000)  # Run the app on all available IP addresses
+    app.run(host='0.0.0.0', port=4000, threaded=True, debug=True)  # Run the app on all available IP addresses
